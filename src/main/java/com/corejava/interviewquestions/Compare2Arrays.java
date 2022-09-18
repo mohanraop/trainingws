e5e933160caca28da41cc8b0a25ca09e1aadaff1
Mohan <mohanrao360@gmail.com>
2022-09-18 23:14:37 +0530

    added

--- src/main/java/com/corejava/interviewquestions/Compare2Arrays.java
+++ src/main/java/com/corejava/interviewquestions/Compare2Arrays.java
@@ -1,6 +1,8 @@
 package com.corejava.interviewquestions;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Compare2Arrays {
     public static void main(String[] args) {
@@ -10,6 +12,52 @@ public class Compare2Arrays {
         int c[]={12,1,2,21};
         int d[]={2,1,1,12};
         System.out.println(check(c,d,4));
+    }
+    public static boolean check2(long A[],long B[],int N)
+    {
+        //using a HashMap to store frequency of elements.
+        HashMap<Long, Long> hm = new HashMap<Long, Long>();
+
+        //incrementing frequencies of elements present in first array in HashMap.
+        for(int i = 0;  i< N; i++)
+        {
+            long num = A[i];
+            if(hm.containsKey(num))
+            {
+                long freq = hm.get(num);
+                freq++;
+                hm.put(num, freq);
+            }
+            else{
+                hm.put(num, (long)1);
+            }
+        }
+        //decrementing frequencies of elements present in
+        //second array in the HashMap.
+        for(int i = 0; i < N; i++)
+        {
+            long num = B[i];
+            if(hm.containsKey(num))
+            {
+                long freq = hm.get(num);
+                freq--;
+                hm.put(num, freq);
+            }
+        }
+
+        //iterating over the HashMap.
+        for(Map.Entry<Long, Long> entry: hm.entrySet())
+        {
+            //if frequency of any element in HashMap now is not zero it means
+            //that its count in two arrays was not equal so arrays aren't equal.
+            if(entry.getValue() != 0)
+            {
+                //returning false since arrays are not equal.
+                return false;
+            }
+        }
+        //returning true if arrays are equal.
+        return true;
     }
     public static boolean check(int a[],int b[],int n)
     {
